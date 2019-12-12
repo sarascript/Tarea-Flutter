1724df4793139d65a4ae6c088c8712b2522050f8
sarascript <correasg.sara@gmail.com>
2019-12-12 13:09:20 +0100

    Step 6

--- lib/main.dart
+++ lib/main.dart
@@ -77,7 +77,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Flutter layout demo'),
         ),
-        body: Column(
+        body: ListView(
             children: [
               Image.asset(
                   'images/lake.jpg',
